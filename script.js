a391596b2b5dbb1a5d39d0dcee5be86ddb94a5ad
fabio231212 <ramirezrojasfabio@gmail.com>
2022-07-12 22:03:37 -0600

    menu

---
+++ script.js
@@ -0,0 +1,12 @@
+function filtroMenu(c) {
+    var x = document.getElementById("todo");
+    var y = x.querySelectorAll("div");
+    var i,c;
+    for (i = 0; i < y.length; i++) {
+    if (y[i].className == c || c == "all") 
+    {
+        y[i].style.display = "inline-block"
+    }
+    else {y[i].style.display = "none"}
+    }
+    }
