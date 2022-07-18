dee177220e819bb11abda1ed42c4ff1d7fd7df6f
alfres2503 <lusuarezag@est.utn.ac.cr>
2022-07-17 18:55:11 -0600

    Migré algunos estilos al css, metí una scroll bar pichuda y otros detallillos

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function filtroMenu(c) {
     var x = document.getElementById("todo");
     var y = x.querySelectorAll("div");
-    var i,c;
+    var i, c;
     for (i = 0; i < y.length; i++) {
-    if (y[i].className == c || c == "all") 
-    {
-        y[i].style.display = "inline-block"
+        if (y[i].className == c || c == "all") {
+            y[i].style.display = "inline-block";
+        } else {
+            y[i].style.display = "none";
+        }
     }
-    else {y[i].style.display = "none"}
-    }
-    }
+}
