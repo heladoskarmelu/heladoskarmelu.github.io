07edaf9b39977c3ac15a03800163cf578d369a30
fabio231212 <ramirezrojasfabio@gmail.com>
2022-07-18 20:39:38 -0600

    menu-helados-60%

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function filtroMenu(c) {
     var x = document.getElementById("todo");
-    var y = x.querySelectorAll("div");
+    var y = x.querySelectorAll("li");
+    // console.log(c);
+    // console.log(y);
     var i, c;
     for (i = 0; i < y.length; i++) {
         if (y[i].className == c || c == "all") {
